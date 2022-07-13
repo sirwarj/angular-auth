ea48b243441d7470f46e3b5854c9325ea13c0ecd
Warawit Siriyong <warawit.j@gmail.com>
2022-07-13 18:50:12 +0700

    Learn auth

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  form: FormGroup | any
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: '',
+      password: '',
+    })
+  }
+
+  submit(): void {
+    this.http.post('http://localhost:8080/api/login', this.form.getRawValue(), {
+      withCredentials: true
+    }).subscribe(() => this.router.navigate(['/']));
+  }
+}
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  form: FormGroup | any
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+    ) { }
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: '',
+      email: '',
+      password: ''
+    });
+  }
+
+  submit(): void {
+    this.http.post('http://localhost:8080/api/register', this.form.getRawValue())
+    .subscribe(() => this.router.navigate(['/login']));
+  }
+}
